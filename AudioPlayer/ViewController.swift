434dc712c1413b208202c6aca04314a9edd7b556
Michael Miles <michael.gage.miles@gmail.com>
2018-12-21 18:20:10 -0800

    Initial Commit

---
+++ AudioPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudioPlayer
+//
+//  Created by Michael Miles on 12/21/18.
+//  Copyright © 2018 Michael Miles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
